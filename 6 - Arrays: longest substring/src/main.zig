59341e091aa45aacaef72dcb7bf2fddbd2d3e1fc
Eduard G.Castelló <edugarcas@gmail.com>
2025-11-13 19:44:26 +0100

    longest substrings

--- 6 - Arrays: longest substring/src/main.zig
+++ 6 - Arrays: longest substring/src/main.zig
@@ -10,4 +10,19 @@ pub fn main() !void {
 
     const elems = [_]u8{ 'a', 'c', 'c', 'd', 'e' };
     var subMap = std.hash_map.AutoHashMap(u8, usize).init(alloc);
+
+    //var ans: usize = 0;
+    var left: usize = 0;
+    var right: usize = 0;
+
+    for (elems, 0..) |elem, i| {
+        if (subMap.get(elem)) |e| {
+            const a: f64 = @floatFromInt(left);
+            const b: f64 = @floatFromInt(e + 1);
+            left = @intFromFloat(@max(a, b));
+        }
+        right += 1;
+
+        try subMap.put(elem, i);
+    }
 }
