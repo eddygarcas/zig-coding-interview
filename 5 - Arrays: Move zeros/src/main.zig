bf46a3a7227802483fa6121942b4df4b36066334
duard <eduard@rzilient.club>
2025-11-14 16:41:20 +0100

    longest substring with timer

--- 5 - Arrays: Move zeros/src/main.zig
+++ 5 - Arrays: Move zeros/src/main.zig
@@ -4,14 +4,15 @@ pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
 
     var elements = [8]u8{ 6, 1, 0, 3, 0, 1, 0, 12 };
-    const elem_ptr: [*]u8 = &elements;
+    // Here we are going to use a pointer to an array
+    const elem_ptr: *[8]u8 = &elements;
 
     const start = std.time.nanoTimestamp();
     var position: usize = 0;
 
     for (elements[0..]) |element| {
         if (element != 0) {
-            elem_ptr[position] = element;
+            elem_ptr.*[position] = element;
             position += 1;
         }
     }
--- 6 - Arrays: longest substring/src/main.zig
+++ 6 - Arrays: longest substring/src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
+    var timer = try std.time.Timer.start();
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var allocator = std.heap.ArenaAllocator.init(gpa.allocator());
     defer allocator.deinit();
@@ -10,19 +11,27 @@ pub fn main() !void {
 
     const elems = [_]u8{ 'a', 'c', 'c', 'd', 'e' };
     var subMap = std.hash_map.AutoHashMap(u8, usize).init(alloc);
+    defer subMap.deinit();
 
-    //var ans: usize = 0;
+    var ans: usize = 0;
     var left: usize = 0;
     var right: usize = 0;
 
     for (elems, 0..) |elem, i| {
         if (subMap.get(elem)) |e| {
-            const a: f64 = @floatFromInt(left);
-            const b: f64 = @floatFromInt(e + 1);
-            left = @intFromFloat(@max(a, b));
+            left = @max(left, e + 1);
         }
         right += 1;
 
         try subMap.put(elem, i);
+        ans = @max(right - left, ans);
     }
+    const final: f64 = @floatFromInt(timer.lap() / std.time.ns_per_ms);
+
+    std.debug.print("Time elapsed: {d:.3}ns\n", .{final});
+    var it = subMap.iterator();
+    while (it.next()) |kv| {
+        std.debug.print("{},", .{kv.value_ptr.*});
+    }
+    std.debug.print("\nMax substring {d}\n", .{ans});
 }
