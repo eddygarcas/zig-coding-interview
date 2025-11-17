c0ba335ec6794720ad2ca545d81ed5d32f1cd3c9
Eduard G.Castelló <edugarcas@gmail.com>
2025-11-17 16:22:44 +0100

    Implement the lowest first and last approach next will use binary search

--- 7 - Arrays: First and last position/src/main.zig
+++ 7 - Arrays: First and last position/src/main.zig
@@ -1,16 +1,31 @@
 const std = @import("std");
 
 const point = struct {
-    x: i4,
-    y: i4,
+    x: i8,
+    y: i8,
 
-    fn toArray(this: point) [2]i4 {
+    fn toArray(this: point) [2]i8 {
         return .{ this.x, this.y };
     }
 };
 
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
-    const p: point = point{ .x = -1, .y = -1 };
+    var p: point = point{ .x = -1, .y = -1 };
+    const nums = [_]u8{ 10, 11, 11, 11, 12, 13, 18, 19, 19, 19, 21, 22, 23, 24, 25, 26, 26 };
     std.debug.print("Point array {any}\n", .{p.toArray()});
+
+    const target: u8 = 19;
+
+    for (nums, 0..) |num, i| {
+        if (num == target) {
+            if (p.x == -1) {
+                p = point{ .x = @intCast(i), .y = @intCast(i) };
+            } else {
+                p.y = @intCast(i);
+            }
+        }
+    }
+
+    std.debug.print("First and last position is {d} and {d}\n", .{ p.x, p.y });
 }
