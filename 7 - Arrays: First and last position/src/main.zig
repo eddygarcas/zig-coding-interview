513b1300a75cd539315165c13612694b3ada898b
Eduard G.Castelló <edugarcas@gmail.com>
2025-11-18 17:13:31 +0100

    first and last position

--- 7 - Arrays: First and last position/src/main.zig
+++ 7 - Arrays: First and last position/src/main.zig
@@ -17,6 +17,7 @@ pub fn main() !void {
 
     const target: u8 = 19;
 
+    var start = try std.time.Timer.start();
     for (nums, 0..) |num, i| {
         if (num == target) {
             if (p.x == -1) {
@@ -26,6 +27,47 @@ pub fn main() !void {
             }
         }
     }
+    var elapsed = start.read();
+    std.debug.print("Time elapsed using strcut version: {d}ns\n", .{elapsed});
 
+    start = try std.time.Timer.start();
+    p.x = findFirst(&nums, target);
+    p.y = findLast(&nums, target);
+    elapsed = start.read();
+    std.debug.print("Time elapsed binary search: {d}ns\n", .{elapsed});
     std.debug.print("First and last position is {d} and {d}\n", .{ p.x, p.y });
+}
+
+pub fn findFirst(nums: []const u8, target: u8) i8 {
+    var left: u8 = 0;
+    var right: u8 = @intCast(nums.len);
+    while (right >= left) {
+        const mid: u8 = (right + left) / 2;
+        if (nums[mid] == target) {
+            if (mid == 0 or nums[mid - 1] != target) return @intCast(mid);
+            right = mid - 1;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return -1;
+}
+
+pub fn findLast(nums: []const u8, target: u8) i8 {
+    var left: u8 = 0;
+    var right: u8 = @intCast(nums.len);
+    while (right >= left) {
+        const mid: u8 = (right + left) / 2;
+        if (nums[mid] == target) {
+            if (mid == @as(u8, @intCast(nums.len)) or nums[mid + 1] != target) return @intCast(mid);
+            left = mid + 1;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return -1;
 }
