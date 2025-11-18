2d388ccd9721fe04530be35b11ec506413543751
Eduard G.Castelló <edugarcas@gmail.com>
2025-11-18 17:32:54 +0100

    first and last position optimization

--- 7 - Arrays: First and last position/src/main.zig
+++ 7 - Arrays: First and last position/src/main.zig
@@ -29,45 +29,61 @@ pub fn main() !void {
     }
     var elapsed = start.read();
     std.debug.print("Time elapsed using strcut version: {d}ns\n", .{elapsed});
+    std.debug.print("First and last position using stryct are {d} and {d}\n", .{ p.x, p.y });
 
     start = try std.time.Timer.start();
     p.x = findFirst(&nums, target);
     p.y = findLast(&nums, target);
     elapsed = start.read();
-    std.debug.print("Time elapsed binary search: {d}ns\n", .{elapsed});
-    std.debug.print("First and last position is {d} and {d}\n", .{ p.x, p.y });
+    std.debug.print("\nTime elapsed binary search: {d}ns\n", .{elapsed});
+    std.debug.print("First and last position using binary search are {d} and {d}\n", .{ p.x, p.y });
 }
 
 pub fn findFirst(nums: []const u8, target: u8) i8 {
+    if (nums.len == 0) return -1;
+
     var left: u8 = 0;
     var right: u8 = @intCast(nums.len);
-    while (right >= left) {
+
+    var result: i8 = -1;
+
+    loop: while (right >= left) {
         const mid: u8 = (right + left) / 2;
         if (nums[mid] == target) {
-            if (mid == 0 or nums[mid - 1] != target) return @intCast(mid);
+            //if (mid == 0 or nums[mid - 1] != target) return @intCast(mid);
+            result = @as(i8, @intCast(mid));
+            if (mid == 0) break :loop;
             right = mid - 1;
         } else if (nums[mid] > target) {
+            if (mid == 0) break :loop;
             right = mid - 1;
         } else {
             left = mid + 1;
         }
     }
-    return -1;
+    return result;
 }
 
 pub fn findLast(nums: []const u8, target: u8) i8 {
+    if (nums.len == 0) return -1;
+
     var left: u8 = 0;
     var right: u8 = @intCast(nums.len);
-    while (right >= left) {
+
+    var result: i8 = -1;
+
+    loop: while (right >= left) {
         const mid: u8 = (right + left) / 2;
         if (nums[mid] == target) {
-            if (mid == @as(u8, @intCast(nums.len)) or nums[mid + 1] != target) return @intCast(mid);
+            //if (mid == @as(u8, @intCast(nums.len)) or nums[mid + 1] != target) return @intCast(mid);
+            result = @as(i8, @intCast(mid));
             left = mid + 1;
         } else if (nums[mid] > target) {
+            if (mid == 0) break :loop;
             right = mid - 1;
         } else {
             left = mid + 1;
         }
     }
-    return -1;
+    return result;
 }
