d213361a8402c12dfa3034c0188be90925242d4f
duard <eduard@rzilient.club>
2025-11-20 17:41:37 +0100

    Math: Binary sum

--- 13 - Math: Binary sum/src/main.zig
+++ 13 - Math: Binary sum/src/main.zig
@@ -1,27 +1,32 @@
 const std = @import("std");
-const _13___Math_Binary_sum = @import("_13___Math_Binary_sum");
 
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try _13___Math_Binary_sum.bufferedPrint();
-}
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    const gpa = std.heap.page_allocator;
+    var result = try std.ArrayList(u8).initCapacity(gpa, 5);
+    defer result.deinit(gpa);
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    const num_a = [_]u4{ 1, 0, 1, 1 };
+    const num_b = [_]u4{ 1, 1, 0, 1 };
+
+    // Use isize to avoid adding if ( i > 0)
+    var i: isize = num_a.len;
+    var j: isize = num_b.len;
+    var carry: u8 = 0;
+
+    while (i > 0 or j > 0 or carry != 0) : ({
+        i -= 1;
+        j -= 1;
+    }) {
+        var sum: u8 = carry;
+        if (i > 0) sum += num_a[@intCast(i - 1)];
+        if (j > 0) sum += num_b[@intCast(j - 1)];
+
+        try result.append(gpa, @mod(sum, 2));
+        carry = sum / 2;
+    }
+    std.mem.reverse(u8, result.items);
+
+    std.debug.print("Result binary sum: {any}", .{result.items});
 }
