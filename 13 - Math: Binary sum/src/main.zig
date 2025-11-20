8c1f47f2e3959cc02b29d56d10eca4594b265b74
duard <eduard@rzilient.club>
2025-11-20 16:46:37 +0100

    Next exercises

---
+++ 13 - Math: Binary sum/src/main.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+const _13___Math_Binary_sum = @import("_13___Math_Binary_sum");
+
+pub fn main() !void {
+    // Prints to stderr, ignoring potential errors.
+    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    try _13___Math_Binary_sum.bufferedPrint();
+}
+
+test "simple test" {
+    const gpa = std.testing.allocator;
+    var list: std.ArrayList(i32) = .empty;
+    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
+    try list.append(gpa, 42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
+
+test "fuzz example" {
+    const Context = struct {
+        fn testOne(context: @This(), input: []const u8) anyerror!void {
+            _ = context;
+            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
+            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+        }
+    };
+    try std.testing.fuzz(Context{}, Context.testOne, .{});
+}
