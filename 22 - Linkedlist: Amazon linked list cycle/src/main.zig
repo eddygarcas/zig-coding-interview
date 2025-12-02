b90a0befdac5326717f3d5e3e5a2ae63e96dc699
Eduard G.Castelló <edugarcas@gmail.com>
2025-12-02 15:35:23 +0100

    LinkedList: Amazon linked list cycle

--- 22 - Linkedlist: Amazon linked list cycle/src/main.zig
+++ 22 - Linkedlist: Amazon linked list cycle/src/main.zig
@@ -13,7 +13,7 @@ const Entry = struct {
 
 // main demonstrates the Floyd's cycle detection algorithm (Tortoise and Hare)
 // by creating a linked list with a cycle and checking for a cycle's entry point.
-// Also that recursive functions are way faster than tha loop to resolve this challenge
+// Also that recursive functions are not guaranteed to be faster in Zig, as recursive calls are added to the stack.
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
@@ -36,21 +36,39 @@ pub fn main() !void {
         switch (i) {
             3 => {
                 cycle = node;
-                node1 = node;
             },
             7 => {
-                node1 = cycle;
+                node.next = cycle;
             },
-            else => {
-                node1 = node;
-            },
+            else => {},
         }
+        node1 = node;
     }
 
+    const result = floydAlg(head);
+    std.debug.print("Floyd Algorithm result {?d}\n", .{result.pos});
     // becuase we've create a cycle list will have to check visited nodes to avoid deallocate twice.
     destroyNodes(allocator, head);
 }
 
+// floydAlg implements Floyd's cycle detection algorithm (Tortoise and Hare).
+// It takes two pointers (tortoise and hare) and checks for a cycle in the linked list.
+// Returns the value at the node where the cycle is detected and true if a cycle exists,
+// otherwise returns 0 and false.
+fn floydAlg(node: *Entry) struct { result: bool, pos: ?usize } {
+    var head: ?*Entry = node;
+    var tail: ?*Entry = node;
+
+    while (head != null and head.?.next != null) {
+        head = head.?.next.?.next;
+        tail = tail.?.next;
+        std.debug.print("Values {?d} {?d}\n", .{ head.?.value, tail.?.value });
+        if (tail == head) return .{ .result = true, .pos = tail.?.value };
+    }
+    return .{ .result = false, .pos = 0 };
+}
+
+// Will destroy the nodes taking care of the fact that its a cycle list, and avoid double deallocate.
 fn destroyNodes(allocator: std.mem.Allocator, head: *Entry) void {
     var visited = std.AutoHashMap(*Entry, void).init(allocator);
     defer visited.deinit();
