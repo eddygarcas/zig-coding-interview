cf6c182a6932978352bb00d6c695365e1ddbe863
Eduard G.Castelló <edugarcas@gmail.com>
2025-12-02 13:15:48 +0100

    LinkedList: Amazon cycle list

--- 22 - Linkedlist: Amazon linked list cycle/src/main.zig
+++ 22 - Linkedlist: Amazon linked list cycle/src/main.zig
@@ -3,30 +3,13 @@ const std = @import("std");
 const Entry = struct {
     value: ?usize = null,
     next: ?*Entry = null,
+    const Self = @This();
+
+    fn init(self: *Self, value: ?usize, next: ?*Entry) void {
+        self.value = value;
+        self.next = next;
+    }
 };
-
-fn ListNode() type {
-    return struct {
-        item: Entry,
-        allocator: std.mem.Allocator,
-
-        const Self = @This();
-
-        fn init(allocator: std.mem.Allocator, value: ?usize, next: ?*Entry) !Self {
-            return .{
-                .item = .{
-                    .value = value,
-                    .next = next,
-                },
-                .allocator = allocator,
-            };
-        }
-        fn set(self: *Self, next: ?*Entry) bool {
-            self.item.next = next;
-            return next != null;
-        }
-    };
-}
 
 // main demonstrates the Floyd's cycle detection algorithm (Tortoise and Hare)
 // by creating a linked list with a cycle and checking for a cycle's entry point.
@@ -39,15 +22,47 @@ pub fn main() !void {
         const leaked = gpa.deinit();
         std.debug.assert(leaked == .ok);
     }
-    var first_item = try ListNode().init(allocator, null, null);
-    std.debug.print("first element {?d}\n", .{first_item.item.value});
 
-    switch (first_item.set(null)) {
-        true => {
-            std.debug.print("Insert", .{});
-        },
-        else => {
-            std.debug.print("No element", .{});
-        },
+    var node1 = try allocator.create(Entry);
+
+    const head = node1;
+    var cycle = node1;
+
+    // Create a cycle list where last node will point to the thrird node.
+    for (0..8) |i| {
+        const node = try allocator.create(Entry);
+        node.init(i, null);
+        node1.next = node;
+        switch (i) {
+            3 => {
+                cycle = node;
+                node1 = node;
+            },
+            7 => {
+                node1 = cycle;
+            },
+            else => {
+                node1 = node;
+            },
+        }
+    }
+
+    // becuase we've create a cycle list will have to check visited nodes to avoid deallocate twice.
+    destroyNodes(allocator, head);
+}
+
+fn destroyNodes(allocator: std.mem.Allocator, head: *Entry) void {
+    var visited = std.AutoHashMap(*Entry, void).init(allocator);
+    defer visited.deinit();
+
+    var cur: ?*Entry = head;
+    while (cur) |node| {
+        if (visited.contains(node)) break;
+        visited.put(node, {}) catch return;
+
+        const next = node.next;
+        allocator.destroy(node);
+
+        cur = next;
     }
 }
