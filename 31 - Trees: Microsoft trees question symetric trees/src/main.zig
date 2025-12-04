16706cfa7ba9959f98388ec68c3a1a833baef132
duard <eduard@rzilient.club>
2025-12-04 18:06:10 +0100

    Use queue rather than recursive calls

---
+++ 31 - Trees: Microsoft trees question symetric trees/src/main.zig
@@ -0,0 +1,93 @@
+const std = @import("std");
+
+pub fn TreeMap(comptime V: type) type {
+    return struct {
+        value: V,
+        left: ?*Self = null,
+        right: ?*Self = null,
+        allocator: std.mem.Allocator,
+
+        const Self = @This();
+
+        fn init(allocator: std.mem.Allocator, value: V) *Self {
+            const node = allocator.create(Self) catch @panic("Error");
+            node.* = .{
+                .allocator = allocator,
+                .value = value,
+                .left = null,
+                .right = null,
+            };
+            return node;
+        }
+
+        // isSymetric checks if two binary trees are mirror images of each other
+        // Will implement a version using Queue approach.
+        // Using recursive calls has penalties as every call goes into the stack
+        fn isSymetric(self: *Self) !bool {
+            var queue = try std.ArrayList(struct {
+                l: ?*TreeMap(V),
+                r: ?*TreeMap(V),
+            }).initCapacity(self.allocator, 0);
+
+            defer queue.deinit(self.allocator);
+
+            try queue.append(self.allocator, .{
+                .l = self.left,
+                .r = self.right,
+            });
+
+            while (queue.pop()) |pair| {
+                const l = pair.l;
+                const r = pair.r;
+
+                if (l == null and r == null) continue;
+                if (l == null or r == null) return false;
+
+                if (l.?.value != r.?.value) return false;
+
+                try queue.append(self.allocator, .{
+                    .l = l.?.left,
+                    .r = r.?.right,
+                });
+
+                try queue.append(self.allocator, .{
+                    .l = l.?.right,
+                    .r = r.?.left,
+                });
+            }
+            return true;
+        }
+
+        // You can pass *TreeMap(V) or *Self will work either way
+        pub fn deinit(self: *Self) void {
+            if (self.left) |l| l.deinit();
+            if (self.right) |r| r.deinit();
+            self.allocator.destroy(self);
+        }
+    };
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    defer {
+        const leaked = gpa.deinit();
+        std.debug.assert(leaked == .ok);
+    }
+
+    var root = TreeMap(usize).init(allocator, 1);
+    defer root.deinit();
+
+    root.left = TreeMap(usize).init(allocator, 2);
+    root.right = TreeMap(usize).init(allocator, 2);
+
+    root.left.?.left = TreeMap(usize).init(allocator, 3);
+    root.left.?.right = TreeMap(usize).init(allocator, 4);
+
+    root.right.?.left = TreeMap(usize).init(allocator, 4);
+    root.right.?.right = TreeMap(usize).init(allocator, 3);
+
+    const result = try root.isSymetric();
+    std.debug.print("Tree is symetric? {}", .{result});
+}
