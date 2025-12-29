11e54a6b5a842ca021709ea3df83c407f59b366f
duard <eduard@rzilient.club>
2025-12-29 20:00:03 +0100

    Backtracking subset

--- 27 - Backtracking: facebook backtracking subsets/src/main.zig
+++ 27 - Backtracking: facebook backtracking subsets/src/main.zig
@@ -11,11 +11,7 @@ const Result = struct {
         };
     }
 
-    pub fn append(self: *Result, elements: []u32) !void {
-        try self.subset.append(self.allocator, elements);
-    }
-
-    pub fn append_items(self: *Result, elements: []const u32) !void {
+    pub fn append(self: *Result, elements: []const u32) !void {
         const snapshot = try self.allocator.alloc(u32, elements.len);
         @memcpy(snapshot, elements);
         try self.subset.append(self.allocator, snapshot);
@@ -44,29 +40,29 @@ pub fn main() !void {
     var cur = try std.ArrayList(u32).initCapacity(allocator, 0);
     defer cur.deinit(allocator);
 
-    var nums = try allocator.alloc(u32, 3);
+    const nums = try allocator.alloc(u32, 3);
     defer allocator.free(nums);
     @memcpy(nums, &[_]u32{ 1, 2, 3 });
 
-    try recurSubset(&nums, &result, &cur, 0, allocator);
+    try recurSubset(nums, &result, &cur, 0, allocator);
     std.debug.print("Answer: {any}\n", .{result.subset.items});
 }
 
-fn recurSubset(nums: *[]u32, ans: *Result, cur: *std.ArrayList(u32), index: usize, allocator: std.mem.Allocator) !void {
+fn recurSubset(nums: []const u32, ans: *Result, cur: *std.ArrayList(u32), index: usize, allocator: std.mem.Allocator) !void {
     // We could say nums.len as Zig dereference but here it's better to be explicit.
-    if (index > nums.*.len) return;
+    if (index > nums.len) return;
 
     // We can't append items diretly to the Result items struct, we need to allocate and make a
     // copy first. So will pass items and then will make a memory copy of the array to be able to
     // add it as part of the answer.
-    try ans.append_items(cur.items);
+    try ans.append(cur.items);
     var i = index;
-    while (i < nums.*.len) : (i += 1) {
-        if (i > index and nums.*[i] == nums.*[i - 1]) {
+    while (i < nums.len) : (i += 1) {
+        if (i > index and nums[i] == nums[i - 1]) {
             continue;
         }
         //Add my choice to the current subset as valid answer
-        try cur.append(allocator, nums.*[i]);
+        try cur.append(allocator, nums[i]);
         // Exaplore all the remaining subsets
         try recurSubset(nums, ans, cur, i + 1, allocator);
         //Backtracking: I'm done exploring with this number, remove it and go back.
