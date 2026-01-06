be437cc1858d2d32e4e9b9fb8e9e3a923465d5f8
duard <edugarcas@gmail.com>
2026-01-06 18:56:05 +0100

    Stack: Valid parenthesis

--- 39 - Stack: valid parenthesis/src/main.zig
+++ 39 - Stack: valid parenthesis/src/main.zig
@@ -3,32 +3,41 @@ const std = @import("std");
 fn Stack(comptime T: type) type {
     return struct {
         const Self = @This();
-        items: []T,
+        items: std.ArrayList(T),
         match: std.AutoArrayHashMap(T, T),
         allocator: std.mem.Allocator,
 
-        pub fn init(allocator: std.mem.Allocator) Stack(T) {
+        pub fn init(allocator: std.mem.Allocator) !Stack(T) {
             return .{
-                .items = try allocator.alloc(T, 5),
+                .items = try std.ArrayList(T).initCapacity(allocator, 0),
                 .allocator = allocator,
                 .match = std.AutoArrayHashMap(T, T).init(allocator),
             };
         }
 
         pub fn pop(self: *Stack(T)) T {
-            const elem = self.items[self.items.len];
-            self.items = self.items[self.items.len - 1];
+            const elem = self.items.pop().?;
+            std.debug.print("Element : {d}\n", .{elem});
             return elem;
         }
-        pub fn isValid(self: *Stack(T), input: []const T) bool {
+
+        pub fn isValid(self: *Stack(T), input: []const T) !bool {
             for (input) |byte| {
                 if (self.match.get(byte)) |e| {
-                    self.items[self.items.len] = e;
-                } else {}
+                    std.debug.print("Pushing {any}\n", .{e});
+                    try self.items.append(self.allocator, e);
+                } else {
+                    std.debug.print("Popping {any}\n", .{byte});
+                    if (self.match.get(self.pop()) != byte) {
+                        return false;
+                    }
+                }
             }
+            return true;
         }
 
         pub fn deinit(self: *Stack(T)) void {
+            self.items.deinit(self.allocator);
             self.match.deinit();
         }
     };
@@ -44,9 +53,12 @@ pub fn main() !void {
     }
 
     const input = &[_]u8{ '{', '(', '[', ']', ')', '}' };
-    var stack = Stack(u8).init(allocator, input);
+    std.debug.print("Input : {any}\n", .{input});
+    var stack = try Stack(u8).init(allocator);
     defer stack.deinit();
     try stack.match.put('(', ')');
     try stack.match.put('[', ']');
     try stack.match.put('{', '}');
+    const result = stack.isValid(input);
+    std.debug.print("Result : {any}\n", .{result});
 }
